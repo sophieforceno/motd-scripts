50f1cba3718c5327b3086e149551c17dfa531801
Andy Forceno <aforceno@protonmail.com>
2019-07-27 13:44:07 -0400

    Added sudo to command

--- 95-fail2ban.sh
+++ 95-fail2ban.sh
@@ -10,7 +10,7 @@ if [[ $(which fail2ban-server) ]]; then
 	IFS=$'\n'
 
 #	echo "  Banned IPs:"
-	bannedCount=( $(awk '($(NF-1) = /Ban/){ print $1 "\t" $NF }' "$f2blog" | grep "$(date +%Y-%m-%d)" | awk '{ print $2 }' | uniq | wc -l) ) 
+	bannedCount=( $(sudo awk '($(NF-1) = /Ban/){ print $1 "\t" $NF }' "$f2blog" | grep "$(date +%Y-%m-%d)" | awk '{ print $2 }' | uniq | wc -l) ) 
 #	echo ${BANNED[@]}
 #	for ip in ${BANNED[@]}; do
 		#echo "  $ip"
