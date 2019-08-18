7aa9c5de9210a31ae154249f9214bec8d4463a78
Andy Forceno <aforceno@protonmail.com>
2019-08-17 21:38:16 -0400

    Improved temperature parsing

--- 30-temperature.sh
+++ 30-temperature.sh
@@ -4,10 +4,13 @@
 # Adapted from: https://github.com/RIKRUS/MOTD
 #
 
+# TODO: Get temps and types from /sys/class/thermal/thermal_zone0/temp
+#		and /sys/class/thermal/thermal_zone0/type
+
 if [ -n $(which sensors) ]; then
 	source $HOME/.config/motd.conf
-	CORES=( $(sensors | grep 'Core' | awk ' { print $2 }' | tr -d ':')  )
-	TEMPS=( $(sensors | grep 'Core' | awk -F : ' { print $2 }' | cut -d' ' -f10 | tr -d '+°C' | cut -d '.' -f1) )
+	CORES=( $(sensors | grep -E 'Core' | awk ' { print $2 }' | tr -d ':')  )
+	TEMPS=( $(sensors | awk '{ if ($1=="SoC" || $1=="Core") print $3 }' | tr -d '+°C' | cut -d '.' -f1) )
 	gpuTemp=$(sensors | awk 'f { print $2; f=0 } /GPU/{f=1}' | tr -d '+°C')
 	gpuTemp=$(echo ${gpuTemp%.*})
 	miscTemp=$(sensors | grep -E '_thermal|temp1|temp2' | awk '{ print $2 }' | tr -d '+°C' | head -1)
