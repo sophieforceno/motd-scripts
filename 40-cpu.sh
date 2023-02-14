bed3790e521562bb8b66df28103f79ea9587acd3
sophiejane <3803896+sophieforceno@users.noreply.github.com>
2023-02-13 19:17:29 -0500

    Update 40-cpu.sh

--- 40-cpu.sh
+++ 40-cpu.sh
@@ -10,15 +10,19 @@ barclear=""
 
 percentUsed=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) " "; }' <(grep -w "cpu" /proc/stat) <(sleep 0.5;grep -w "cpu" /proc/stat))
 percentUsed=$(echo ${percentUsed%.*})
-
+percentUsed="40"
 usedBarWidth=$((($percentUsed*$barWidth)/100))
 barContent=""
 color="38;5;36m"
 
-if [[ "${percentUsed}" -ge 70 && "${percentUsed}" -lt 89 ]]; then
-  color="38;5;129m"
-elif [[ "${percentUsed}" -ge 90 && "${percentUsed}" -le 100 ]]; then
-  color="38;5;196m"
+if [[ "${percentUsed}" -gt 44 && "${percentUsed}" -le 59 ]]; then
+	color="38;5;53m"
+elif [[ "${percentUsed}" -gt 60 && "${percentUsed}" -le 75 ]]; then
+	color="38;5;127m"
+  #color="38;5;129m"
+elif [[ "${percentUsed}" -gt 75 && "${percentUsed}" -le 100 ]]; then
+	color="38;5;198m"
+  #color="38;5;196m"
 fi
 
 for sep in $(seq 1 $usedBarWidth); do
