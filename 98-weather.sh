7be0613a8871b74905abbd5d30bfa70a6b1e3dc4
sophiejane <3803896+sophieforceno@users.noreply.github.com>
2023-05-27 23:54:30 -0400

    Create 98-weather.sh
    New script that returns current weather conditions

---
+++ 98-weather.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+source $HOME/.config/motd.conf
+
+getdate() { 
+	date | awk '{print $1, $2, $3}'
+}
+
+getweather(){ 
+	curl -s -f "https://wttr.in/$location?format=%l%20%t%20%h%20%w%20%p%20%u%20%c"| column -t
+}
+
+weather=$(getweather)
+
+echo -n "  Current weather conditions | "; getdate
+echo "    Location    Temp. Humid. Wind Precip. UV Condition"
+echo -e "   \e[0;37m $weather \e[0m\n"%
