94d4b80646ff3dd2279da0c9f84dc68feaac3e67
sophiejane <3803896+sophieforceno@users.noreply.github.com>
2023-05-28 22:38:52 -0400

    Update 98-weather.sh: Fixed input too long error
    Fixed "input is too long" error from the column command

--- 98-weather.sh
+++ 98-weather.sh
@@ -3,7 +3,9 @@
 source $HOME/.config/motd.conf
 
 wttrfmt="%l%20%t%20%h%20%w%20%p%20%u%20%c"
-weather=$(curl -s -f "https://wttr.in/$location?format=$wttrfmt"| column -t)
+weather=$(curl -s -f "https://wttr.in/$location?format=$wttrfmt")
+# A newline is required at end of input in some versions of the column command
+weather=$(echo -e "$weather\n" | column -t)
 
 echo -n "  Weather conditions |"; echo -e "\e[0;37m $(date '+%a %b %d %H:%M') \e[0m"
 echo "    Location    Temp. Humid. Wind Precip. UV Condition"
