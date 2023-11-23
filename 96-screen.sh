124b196c4085842fcbe21a5addcc8490c52c338c
sophiejane <3803896+sophieforceno@users.noreply.github.com>
2023-11-23 00:47:16 -0500

    Create 96-screen.sh
    Shows running screen sessions (attached/detached)

---
+++ 96-screen.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# Prints list of running screen sessions
+# Put a green or red dot in front depending on whether attached or detached
+
+if [[ $(which screen 2> /dev/null) ]]; then
+	if [[ "$(screen -r | wc -l)" > 2 ]]; then
+		sessions=$(screen -r | grep tached | tr -d '()'| column -t)
+		detached=$(echo "$sessions" | grep "Detached" | sed 's/Detached//g')
+		attached=$(echo "$sessions" | grep "Attached" | sed 's/Attached//g')
+		printf "  Screen sessions:\n"   
+        if [[ -n "$detached" ]]; then
+            dot="\e[38;5;198m  ●\e[0m"
+            while IFS= read -r line; do
+                printf "$dot \e[0;37m$line\e[0m\n"
+            done <<< "$detached"
+        fi
+        if [[ -n "$attached" ]]; then
+        	dot="\e[38;5;36m  ●\e[0m"     
+            while IFS= read -r line; do
+                printf "$dot \e[0;37m$line\e[0m\n"
+            done <<< "$attached"
+        fi
+        echo ""
+    fi
+fi
