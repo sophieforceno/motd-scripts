b757d9be29af5e75a6aecd59fc4709b689b6ced3
sophiejane <3803896+sophieforceno@users.noreply.github.com>
2023-12-10 00:42:36 -0500

    Update 96-screen.sh
    Switched screen -r to screen -list to handle cases where only one session is open

--- 96-screen.sh
+++ 96-screen.sh
@@ -1,22 +1,24 @@
 #! /bin/bash
 
 # Prints list of running screen sessions
-# Put a green or red dot in front depending on whether attached or detached
+#
+
+# Put a green or red dot in front depending on whether attached or detatched
 
 if [[ $(which screen 2> /dev/null) ]]; then
-	if [[ "$(screen -r | wc -l)" > 2 ]]; then
-		sessions=$(screen -r | grep tached | tr -d '()'| column -t)
+	if [[ "$(screen -list | wc -l)" > 2 ]]; then
+		sessions=$(screen -list | grep tached | tr -d '()' | column -t)
 		detached=$(echo "$sessions" | grep "Detached" | sed 's/Detached//g')
 		attached=$(echo "$sessions" | grep "Attached" | sed 's/Attached//g')
 		printf "  Screen sessions:\n"   
         if [[ -n "$detached" ]]; then
-            dot="\e[38;5;198m  ●\e[0m"
+            dot="\e[38;5;36m  ●\e[0m"
             while IFS= read -r line; do
                 printf "$dot \e[0;37m$line\e[0m\n"
             done <<< "$detached"
         fi
         if [[ -n "$attached" ]]; then
-        	dot="\e[38;5;36m  ●\e[0m"     
+        	dot="\e[38;5;198m  ●\e[0m"     
             while IFS= read -r line; do
                 printf "$dot \e[0;37m$line\e[0m\n"
             done <<< "$attached"
